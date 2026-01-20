1e5356dad358649b7ab5190ede3a7c0bcb91414b
Sayantan De <desayantan1947@gmail.com>
2026-01-20 21:08:00 +0530

    fixed game issue problem

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('game/', views.indexgame, name='index'),
+    # Separate name for the game page so it doesn't override the main 'index' URL
+    path('game/', views.indexgame, name='game'),
     # The dynamic path:
     path('services/<slug:slug>/', views.service_detail, name='service_detail'),
     path("about-us/", views.about_us, name="about_us"),
