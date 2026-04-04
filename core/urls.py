d8d06b14b2c116cc800436071ccccea584408ea5
Sayantan De <desayantan1947@gmail.com>
2026-04-04 09:45:32 +0530

    updatea

--- core/urls.py
+++ core/urls.py
@@ -14,8 +14,9 @@ urlpatterns = [
     path('api/contact/submit/', views.contact_submit, name='contact_submit'),
     
     # Industry Training System
-    path('training/', views.training_list, name='training_list'),
-    path('training/subfield/<slug:slug>/', views.training_subfield_detail, name='training_subfield_detail'),
-    path('training/enroll/<int:package_id>/', views.training_enroll, name='training_enroll'),
-    path('api/training/validate-referral/', views.validate_referral, name='validate_referral'),
+#     path('training/', views.training_list, name='training_list'),
+#     path('training/subfield/<slug:slug>/', views.training_subfield_detail, name='training_subfield_detail'),
+#     path('training/enroll/<int:package_id>/', views.training_enroll, name='training_enroll'),
+#     path('api/training/validate-referral/', views.validate_referral, name='validate_referral'),
+# 
 ]
